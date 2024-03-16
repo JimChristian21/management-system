fb47722d420caa536487fb1a7e6c87bb78caba58
JimChristian21 <jimchristianedullantes9@gmail.com>
2024-03-17 00:47:20 +0800

    Added inventory Page, and index in the controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\{
+    ProfileController,
+    InventoryController
+};
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -35,6 +38,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/inventory', [InventoryController::class, 'index'])->name('inventory');
 });
 
 require __DIR__.'/auth.php';
