a8d06004a715644e01b71d5188d8719ba21125be
JimChristian21 <jimchristianedullantes9@gmail.com>
2024-03-18 22:34:51 +0800

    Added seeder and update schema

--- database/migrations/2024_01_24_171933_create_products_table.php
+++ database/migrations/2024_01_24_171933_create_products_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
 
             $table->id();
+            $table->string('code', 64)->nullable(FALSE);
             $table->string('title', 64)->nullable(FALSE);
             $table->string('description', 255)->nullable(FALSE);
             $table->integer('quantity')->nullable(FALSE);
---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        Product::create([
+            'code' => '10001000',
+            'title' => 'Product 1',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+        Product::create([
+            'code' => '10001002',
+            'title' => 'Product 2',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+        Product::create([
+            'code' => '10001003',
+            'title' => 'Product 3',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+        Product::create([
+            'code' => '10001004',
+            'title' => 'Product 4',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+        Product::create([
+            'code' => '10001005',
+            'title' => 'Product 5',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+        Product::create([
+            'code' => '10001006',
+            'title' => 'Product 6',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+        Product::create([
+            'code' => '10001007',
+            'title' => 'Product 7',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+        Product::create([
+            'code' => '10001008',
+            'title' => 'Product 8',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+        Product::create([
+            'code' => '10001009',
+            'title' => 'Product 9',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+        Product::create([
+            'code' => '1000110',
+            'title' => 'Product 10',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+        Product::create([
+            'code' => '10001011',
+            'title' => 'Product 11',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+        Product::create([
+            'code' => '10001012',
+            'title' => 'Product 12',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+        Product::create([
+            'code' => '10001013',
+            'title' => 'Product 13',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+        Product::create([
+            'code' => '10001014',
+            'title' => 'Product 14',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+        Product::create([
+            'code' => '10001015',
+            'title' => 'Product 15',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+        Product::create([
+            'code' => '10001016',
+            'title' => 'Product 16',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+        Product::create([
+            'code' => '10001017',
+            'title' => 'Product 17',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+
+        Product::create([
+            'code' => '10001018',
+            'title' => 'Product 18',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+        Product::create([
+            'code' => '10001019',
+            'title' => 'Product 19',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+        Product::create([
+            'code' => '10001020',
+            'title' => 'Product 20',
+            'description' => 'Lorem Ipsum',
+            'quantity' => 10,
+            'price' => 20
+        ]);
+    }
+}
