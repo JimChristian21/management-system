d587f17a4e469db8480717fb3bd6039b41d3aab6
JimChristian21 <jimchristianedullantes9@gmail.com>
2024-03-19 23:39:03 +0800

    added fields for product component

--- resources/js/Pages/Inventory/Product.vue
+++ resources/js/Pages/Inventory/Product.vue
@@ -13,11 +13,15 @@ defineProps({
             <td>Code</td>
             <td>Title</td>
             <td>Description</td>
+            <td>Quantity</td>
+            <td>Product</td>
         </tr>
         <tr v-for="product in products">
             <td>{{ product.code }}</td>
             <td>{{ product.title }}</td>
             <td>{{ product.description }}</td>
+            <td>{{ product.quantity }}</td>
+            <td>{{ product.price }}</td>
         </tr>
     </table>
 </template>
