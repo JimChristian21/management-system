a15035f9951aa0326ebd9ba017248773392b2472
JimChristian21 <jimchristianedullantes9@gmail.com>
2024-03-24 13:11:53 +0800

    Use native approave on table

--- resources/js/Pages/Inventory/Product.vue
+++ resources/js/Pages/Inventory/Product.vue
@@ -1,27 +1,32 @@
 <script setup>
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import { router } from '@inertiajs/vue3';
+
 defineProps({
     'products': Object
 });
-
 </script>
 
 <template>
-    {{ console.log(products[0]) }}
-    <h1>TEST</h1>
     <table>
         <tr>
             <td>Code</td>
             <td>Title</td>
             <td>Description</td>
             <td>Quantity</td>
-            <td>Product</td>
+            <td>Price</td>
+            <td>Action</td>
         </tr>
-        <tr v-for="product in products">
+        <tr v-for="product in products" :key="product.code">
             <td>{{ product.code }}</td>
             <td>{{ product.title }}</td>
             <td>{{ product.description }}</td>
             <td>{{ product.quantity }}</td>
             <td>{{ product.price }}</td>
+            <td>
+
+            </td>
         </tr>
     </table>
 </template>
