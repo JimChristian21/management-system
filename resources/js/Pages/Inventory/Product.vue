5ba0988e351166591a9a7d4e9ef48db06b8786fc
JimChristian21 <jimchristianedullantes9@gmail.com>
2024-03-24 14:04:28 +0800

    Added border

--- resources/js/Pages/Inventory/Product.vue
+++ resources/js/Pages/Inventory/Product.vue
@@ -5,31 +5,32 @@ import { addIcons } from "oh-vue-icons";
 
 addIcons(MdDelete);
 
-
 defineProps({
     'products': Object
 });
 </script>
 
 <template>
-    <table>
-        <tr>
-            <td>Code</td>
-            <td>Title</td>
-            <td>Description</td>
-            <td>Quantity</td>
-            <td>Price</td>
-            <td>Actions</td>
-        </tr>
-        <tr v-for="product in products" :key="product.code">
-            <td>{{ product.code }}</td>
-            <td>{{ product.title }}</td>
-            <td>{{ product.description }}</td>
-            <td>{{ product.quantity }}</td>
-            <td>{{ product.price }}</td>
-            <td>
-                <v-icon name="md-delete" width="20" height="20" class="text-gray-800"/>
-            </td>
-        </tr>
-    </table>
+    <div class="w-full p-3 border-2 border-gray-400 rounded-lg ">
+        <table class="w-full">
+            <tr class="font-bold rounded-tl-full">
+                <td class="py-3 p-2 border-t border-r border-l border-gray-800">Code</td>
+                <td class="py-3 p-2 border-t border-r border-l border-gray-800">Title</td>
+                <td class="py-3 p-2 border-t border-r border-l border-gray-800">Description</td>
+                <td class="py-3 p-2 border-t border-r border-l border-gray-800">Quantity</td>
+                <td class="py-3 p-2 border-t border-r border-l border-gray-800">Price</td>
+                <td class="py-3 p-2 border-t border-r border-l border-gray-800">Actions</td>
+            </tr>
+            <tr v-for="product in products" :key="product.code" class="border border-gray-800">
+                <td class="p-2">{{ product.code }}</td>
+                <td class="p-2">{{ product.title }}</td>
+                <td class="p-2">{{ product.description }}</td>
+                <td class="p-2">{{ product.quantity }}</td>
+                <td class="p-2">{{ product.price }}</td>
+                <td class="p-2">
+                    <v-icon name="md-delete" width="20" height="20" class="text-gray-800"/>
+                </td>
+            </tr>
+        </table>
+    </div>
 </template>
